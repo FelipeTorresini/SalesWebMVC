8a3c4ad6e7e38e9d19252f894316b060379456ad
FelipeTorresini <ftorresini@gmail.com>
2021-11-15 09:21:33 -0300

    Primeiro Model-Controller-View

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Salles web MVC app";
+            ViewData["Message"] = "Salles Web MVC app";
             ViewData["NomeDev"] = "Felipe Torresini";
 
             return View();
@@ -38,7 +38,7 @@ namespace SalesWebMVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SalesWebMVC.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
